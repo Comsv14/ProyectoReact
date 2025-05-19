2f931c72ac6d2beb7efbc006eff28a5392a51c92
Lucas Seiz Valdes <lucascomsv@gmail.com>
2025-05-19 13:17:32 +0200

    fin seccion 4

---
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -0,0 +1,13 @@
+    import Proptypes from 'prop-types';
+    export const FirstApp = ({title}) => {
+        //console.log(props)
+        
+        return(
+        <>
+            <code> {title} Seiz Valdés</code>
+            
+        </>
+        );
+        }
+        
+    
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HelloWorldApp } from './HelloWorldApp';
+import {CounterApp} from './CounterApp';
+import './styles.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot( document.getElementById('root') ).render(
+    <React.StrictMode>
+    <CounterApp value={20}></CounterApp>
+    </React.StrictMode>
+);
